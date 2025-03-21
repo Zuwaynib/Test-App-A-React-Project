0bed74c24add1641cc724ce60126121be54c17e1
Zuwaynib <zainabadeoje@gmail.com>
2025-03-21 21:33:59 +0100

    App: added styling for the app component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
-import './src/App.css';
 
 const Card = () => {
   return (
     <div className = "card">
-        
+        <h3>Experience the vibrant nightlife</h3>
+        <p>Lagos's nightlife is an energetic blend of music and clubs, creating a vibrant after-dark scene.</p>
     </div>
   )
 }
