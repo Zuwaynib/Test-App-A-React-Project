b29cc9f83a21657cda2a2adb067954a057cebd23
Zuwaynib <zainabadeoje@gmail.com>
2025-03-22 12:52:05 +0100

    Card: added alternate classnames to style seperate card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 
-const Card = ({ title, subtitle }) => {
+const Card = ({ title, subtitle, altClass }) => {
   return (
-    <div className="card">
+    <div className={"card " + altClass}>
       <h3>{title}</h3>
       <p>{subtitle}</p>
     </div>
@@ -12,15 +12,19 @@ const Card = ({ title, subtitle }) => {
 function App() {
   return (
     <div className="container">
+      <h1>Top 3 activities to do in Lagos</h1>
       <Card
+        altClass="first-card"
         title="Experience the vibrant nightlife."
         subtitle="Lagos's nightlife is an energetic blend of music and clubs, creating a vibrant after-dark scene."
       />
       <Card
+        altClass="second-card"
         title="Explore Lekki Conservation Centre"
         subtitle="The Lekki Conservation Centre offers a tranquil nature escape with canopy walks and wildlife in busy Lagos."
       />
       <Card
+        altClass="third-card"
         title="Visit the National Museum Lagos"
         subtitle="The National Museum offers a Discovery of Nigeria's heritage through diverse artifacts and cultural history."
       />
