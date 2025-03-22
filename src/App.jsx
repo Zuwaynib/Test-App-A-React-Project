c21673e7425436b58534889eb2736ab2874ee864
Zuwaynib <zainabadeoje@gmail.com>
2025-03-22 13:04:22 +0100

    Card: rendered all card components and updated individual styles

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,23 @@ function App() {
   return (
     <div className="container">
       <h1>Top 3 activities to do in Lagos</h1>
-      <Card
-        altClass="first-card"
-        title="Experience the vibrant nightlife."
-        subtitle="Lagos's nightlife is an energetic blend of music and clubs, creating a vibrant after-dark scene."
-      />
-      <Card
-        altClass="second-card"
-        title="Explore Lekki Conservation Centre"
-        subtitle="The Lekki Conservation Centre offers a tranquil nature escape with canopy walks and wildlife in busy Lagos."
-      />
-      <Card
-        altClass="third-card"
-        title="Visit the National Museum Lagos"
-        subtitle="The National Museum offers a Discovery of Nigeria's heritage through diverse artifacts and cultural history."
-      />
+      <div className="card-div">
+        <Card
+          altClass="first-card"
+          title="Experience the vibrant nightlife."
+          subtitle="Lagos's nightlife is an energetic blend of music and clubs, creating a vibrant after-dark scene."
+        />
+        <Card
+          altClass="second-card"
+          title="Explore Lekki Conservation Centre"
+          subtitle="The Lekki Conservation Centre offers a tranquil nature escape with canopy walks and wildlife in busy Lagos."
+        />
+        <Card
+          altClass="third-card"
+          title="Visit the National Museum Lagos"
+          subtitle="The National Museum offers a Discovery of Nigeria's heritage through diverse artifacts and cultural history."
+        />
+      </div>
     </div>
   );
 }
