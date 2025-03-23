e2ceba6bd4ea7860fb0c13969abc4b5f6342da7b
Zuwaynib <zainabadeoje@gmail.com>
2025-03-23 13:13:25 +0100

    Card: seperated card component, updated App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 import "./App.css";
-
-const Card = ({ title, subtitle, altClass }) => {
-  return (
-    <div className={"card " + altClass}>
-      <h3>{title}</h3>
-      <p>{subtitle}</p>
-    </div>
-  );
-};
+import Card from "./components/Card";
 
 function App() {
   return (
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,1 +1,10 @@
+const Card = ({ title, subtitle, altClass }) => {
+  return (
+    <div className={"card " + altClass}>
+      <h3>{title}</h3>
+      <p>{subtitle}</p>
+    </div>
+  );
+};
 
+export default Card;
