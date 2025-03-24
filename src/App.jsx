4f98637cda054a89fbbbe4c9e369565c1bf11f8d
Zuwaynib <zainabadeoje@gmail.com>
2025-03-24 15:26:43 +0100

    Task: cleared all existing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
 import "./App.css";
-import Card from "./components/Card";
+
 
 function App() {
   return (
-    <div className="container">
-      <h1>Top 3 activities to do in Lagos</h1>
-      <div className="card-div">
-        <Card
-          altClass="first-card"
-          title="Experience the vibrant nightlife."
-          subtitle="Lagos's nightlife is an energetic blend of music and clubs, creating a vibrant after-dark scene."
-        />
-        <Card
-          altClass="second-card"
-          title="Explore Lekki Conservation Centre"
-          subtitle="The Lekki Conservation Centre offers a tranquil nature escape with canopy walks and wildlife in busy Lagos."
-        />
-        <Card
-          altClass="third-card"
-          title="Visit the National Museum Lagos"
-          subtitle="The National Museum offers a Discovery of Nigeria's heritage through diverse artifacts and cultural history."
-        />
-      </div>
-    </div>
+    
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,1 @@
-const Card = ({ title, subtitle, altClass }) => {
-  return (
-    <div className={"card " + altClass}>
-      <h3>{title}</h3>
-      <p>{subtitle}</p>
-    </div>
-  );
-};
 
-export default Card;
