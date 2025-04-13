b8e0b921226d727f7070ba74bc3adc82c78e33a2
Zuwaynib <zainabadeoje@gmail.com>
2025-04-13 09:43:17 +0100

    cleaned up revenue card code

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -5,18 +5,8 @@ import DashboardCard from "./components/DashboardCard";
 import OrdersTable from "./components/OrdersTable";
 import RevenueChart from "./components/RevenueChart";
 import { Chart as ChartJS, defaults } from "chart.js/auto";
-import { Bar, Doughnut, Line } from "react-chartjs-2";
-import SourceData from "./data/sourceData.json";
+import { Bar, Doughnut } from "react-chartjs-2";
 
-defaults.maintainAspectRatio = false;
-defaults.responsive = true;
-
-defaults.plugins.title.display = true;
-defaults.plugins.title.align = "start";
-defaults.plugins.title.color = "#4169E1";
-defaults.plugins.title.font.size = 20;
-defaults.plugins.subtitle.align = "start";
-defaults.plugins.subtitle.color = "#4169E1";
 
 const Dashboard = () => {
   return (
@@ -33,35 +23,7 @@ const Dashboard = () => {
           <div className="middle-section">
             <OrdersTable />
           </div>
-          <div className="revenueChart">
-          <Line
-            data={{
-              labels: SourceData.map((data) => data.label),
-              datasets: [
-                {
-                  label: "Total Revenue",
-                  data: SourceData.map((data) => data.value),
-                  backgroundColor: ["#4169E1"],
-                  tension: 0.5,
-                  borderColor: "#4169E1",
-                  pointStyle: false,
-                },
-              ],
-            }}
-            options={{
-              plugins: {
-                title: {
-                  display: true,
-                  text: "#300000.5",
-                },
-                subtitle: {
-                  display: true,
-                  text: "Total Revenue",
-                }
-              }
-            }}
-          />
-          </div>
+          <RevenueChart />
         </div>
       </main>
     </div>
