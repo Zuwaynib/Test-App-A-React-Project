da68ce2c937fd292753e5e2f0713927760531bc1
Zuwaynib <zainabadeoje@gmail.com>
2025-04-13 09:09:26 +0100

    Updated Revenue Chart

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -4,8 +4,19 @@ import Header from "./components/Header";
 import DashboardCard from "./components/DashboardCard";
 import OrdersTable from "./components/OrdersTable";
 import RevenueChart from "./components/RevenueChart";
-import { Chart as ChartJS } from "chart.js/auto";
+import { Chart as ChartJS, defaults } from "chart.js/auto";
 import { Bar, Doughnut, Line } from "react-chartjs-2";
+import SourceData from "./data/sourceData.json";
+
+defaults.maintainAspectRatio = false;
+defaults.responsive = true;
+
+defaults.plugins.title.display = true;
+defaults.plugins.title.align = "start";
+defaults.plugins.title.color = "#4169E1";
+defaults.plugins.title.font.size = 20;
+defaults.plugins.subtitle.align = "start";
+defaults.plugins.subtitle.color = "#4169E1";
 
 const Dashboard = () => {
   return (
@@ -22,17 +33,35 @@ const Dashboard = () => {
           <div className="middle-section">
             <OrdersTable />
           </div>
-          <Bar
+          <div className="revenueChart">
+          <Line
             data={{
-              labels: ["A", "B", "C"],
+              labels: SourceData.map((data) => data.label),
               datasets: [
                 {
-                  label: "Revenue",
-                  data: [200, 300, 400],
+                  label: "Total Revenue",
+                  data: SourceData.map((data) => data.value),
+                  backgroundColor: ["#4169E1"],
+                  tension: 0.5,
+                  borderColor: "#4169E1",
+                  pointStyle: false,
                 },
               ],
             }}
+            options={{
+              plugins: {
+                title: {
+                  display: true,
+                  text: "#300000.5",
+                },
+                subtitle: {
+                  display: true,
+                  text: "Total Revenue",
+                }
+              }
+            }}
           />
+          </div>
         </div>
       </main>
     </div>
