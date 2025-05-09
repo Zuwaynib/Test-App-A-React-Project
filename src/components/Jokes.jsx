835035baf8d39260ec4ea80a90e8e9afde78d3fa
Zuwaynib <zainabadeoje@gmail.com>
2025-05-09 21:34:31 +0100

    fixed error in Jokes component

--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import JokesData from '../data/jokesData.json';
 
 const Jokes = () => {
-
-    JokesData.map((joke) => {
-        return (
-            <div key={joke.id}>
-                <h1>{joke.id}</h1>
-                <p>{joke.joke}</p>
-                <hr></hr>
-            </div>
-        )
-    })
-}
+    return (
+        <div>
+            {JokesData.map((joke) => (
+                <div key={joke.id}>
+                    <h1>{joke.id}</h1>
+                    <p>{joke.joke}</p>
+                    <hr />
+                </div>
+            ))}
+        </div>
+    );
+};
 
 export default Jokes;
