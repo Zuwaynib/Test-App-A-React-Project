f8fb172be7f54274401d81ac5c78a82173177ded
Zuwaynib <zainabadeoje@gmail.com>
2025-05-10 23:13:32 +0100

    updated Pizza.jsx to show images

--- src/components/Pizzas.jsx
+++ src/components/Pizzas.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import PizzaData from '../data/pizzaData.json';
+import React from "react";
+import PizzaData from "../data/pizzaData.json";
 
 const Pizzas = () => {
-    return (
-        <div>
-            {PizzaData.map((pizza) => (
-                <div key={pizza.id}>
-                    <h1>{pizza.id}</h1>
-                    <p>{pizza.title}</p>
-                    <hr />
-                </div>
-            ))}
+  return (
+    <div>
+      {PizzaData.map((pizza) => (
+        <div key={pizza.id}>
+          <img
+            src={pizza.img.src}
+            alt={pizza.name}
+            style={{ width: "200px", height: "200px" }}
+          />
+          <h1>{pizza.name}</h1>
+          <p>{pizza.description}</p>
+          <hr />
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Pizzas;
