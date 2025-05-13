8dc0381f628d79cfb21a44f522a6d7a111c0d826
Zuwaynib <zainabadeoje@gmail.com>
2025-05-13 14:42:54 +0100

    Count: updated count value with UseState

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Practice = () => {
+    let [count, setCount] = useState(0);
+
+    const decreaseCount = () => {
+        setCount(count--);
+    }
+    const increaseCount = () => {
+        setCount(count++);
+    }
   return (
     <div>
         <h1>Practicing React Use State</h1>
-        <button>Click Me</button>
+        <button aria-label="decrease count" onClick={decreaseCount}>-</button>
+        <h2>{count}</h2>
+        <button aria-label="increase count" onClick={increaseCount}>+</button>
     </div>
   )
 }
