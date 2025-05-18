34879fa3d65076af9568e3975cdc782a87d6c907
Zuwaynib <zainabadeoje@gmail.com>
2025-05-18 17:04:41 +0100

    Form: Handle submit

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 
 const Practice = () => {
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const formElements = event.currentTarget;
+  const formData = new FormData(formElements);
+  const email =formData.get("email");
+  const password = formData.get("password");
+  console.log("Email:", email);
+  console.log("Password:", password);
+  formElements.reset();
+  alert("Form submitted successfully!");
+}
+
   return (
     <main>
       <h1>Event Sign Up Form</h1>
-      <form>
+      <form method="POST" onSubmit={handleSubmit}>
         <label htmlFor="email">Email</label>
         <input id="email" type="email"  name="email" placeholder="janedoe@gmail.com" />
         <br />
