81a8f0b04660eb259060c8522e2502aa5a4f4698
Zuwaynib <zainabadeoje@gmail.com>
2025-05-18 14:45:59 +0100

    Added form details

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 
 const Practice = () => {
-
-return (
+  return (
     <main>
-        <h1>Event Sign Up Form</h1>
-        <form>
-          <input type="text" />
-          <button>Submit</button>
-        </form>
+      <h1>Event Sign Up Form</h1>
+      <form>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email"  name="email" placeholder="janedoe@gmail.com" />
+        <br />
+        <br />
+
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="password" />
+        <br />
+        <br />
+        <button>Submit</button>
+      </form>
     </main>
-)
+  );
 };
 
 export default Practice;
